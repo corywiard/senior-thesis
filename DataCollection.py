11af6255c188cbb525dd8705fb0d9f221402fe04
Cory Wiard <corywiard@Corys-MacBook-Pro.local>
2021-03-01 13:21:57 -0500

    Add markdown team wins and change program name

--- DataCollection.py
+++ DataCollection.py
@@ -31,37 +31,34 @@ def dataCollectorReds(year):
             for stats_variables in player_variables.find_all('th'):
                 variables.append(stats_variables.get_text())
 
-            # turns list into dictionary
-            #variables_dict = dict.fromkeys(variables)
-            # collects all data from table
-            #stats_list = []
-            #player_stats = soup.find('div', attrs={'class': 'ResponsiveTable ResponsiveTable--fixed-left mt5 remove_capitalize'})
-            #for stats in player_stats.find_all('td'):
-                #stats_list.append(stats.get_text())
-
-            # collects name from Table
+            # collects player's name from Table
             names = ['Name']
             name_variable = soup.find('tbody', attrs={'class': 'Table__TBODY'})
             for data in name_variable.find_all('tr'):
                 names.append(data.get_text())
 
+            # Turns list into dictionary
             names_dict_reds = dict.fromkeys(names)
 
-            # collects data in new list
+            # collects player's data into list
             data_row = []
             row_stats = soup.find('table', attrs={'class': 'Table Table--align-right'})
             for info in row_stats.find_all('td'):
                 data_row.append(info.get_text())
 
+            # multi-dimensional list to hold player stats
             multi_list_of_stats = []
+            # list adds variable names to it
             multi_list_of_stats.append(variables[0:17])
             x = 0
+            # while loop to add row of player stats to new list and delete from old list
             while x < len(names):
                 multi_list_of_stats.append(data_row[0:17])
                 del data_row[0:17]
                 x = x + 1
 
             i = 0
+            # for loop to iterate through dictionary adding list index to each key
             for key in names_dict_reds:
                 names_dict_reds[key] = multi_list_of_stats[i]
                 i = i + 1
